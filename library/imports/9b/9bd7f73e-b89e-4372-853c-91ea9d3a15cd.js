56d595ee70ac916791c1c7cfb91cc2b66b3980d4
lm <l1093015248@163.com>
2018-01-30 12:08:07 +0800

    Initial commit

---
+++ library/imports/9b/9bd7f73e-b89e-4372-853c-91ea9d3a15cd.js
@@ -0,0 +1,41 @@
+"use strict";
+cc._RF.push(module, '9bd7fc+uJ5DcoU8keqdOhXN', 'BtnEvents');
+// scripts/BtnEvents.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        button: {
+            default: null,
+            type: cc.Button
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {},
+
+
+    onLoad: function onLoad() {},
+    btnClick: function btnClick() {
+        cc.director.loadScene("game");
+    }
+    // update (dt) {},
+});
+
+cc._RF.pop();
